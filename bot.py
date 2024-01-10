8a69b15b1e079cd73370f707c85c6cdebbe3c95a
Vica Worth <11998960+VicaWorth@users.noreply.github.com>
2024-01-09 21:46:40 -0800

    Fixed

--- bot.py
+++ bot.py
@@ -50,10 +50,10 @@ class myBot(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+    # async def setup_hook(self):
+    #     # This copies the global commands over to your guild.
+    #     self.tree.copy_global_to(guild=MY_GUILD)
+    #     await self.tree.sync(guild=MY_GUILD)
 
 intents = discord.Intents.default()
 intents.message_content = True
